defb6dbda4dba52698a767ca90b4c83c690a3626
miadweb <131885818+miadweb@users.noreply.github.com>
2025-11-21 17:06:14 -0500

    Initial Commit

---
+++ couplesTime/couplesTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  couplesTimeApp.swift
+//  couplesTime
+//
+//  Created by Mahdi Miad on 21/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct couplesTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ couplesTimeTests/couplesTimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  couplesTimeTests.swift
+//  couplesTimeTests
+//
+//  Created by Mahdi Miad on 21/11/2025.
+//
+
+import Testing
+@testable import couplesTime
+
+struct couplesTimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
